3321ae28fe41af74d2787fc83ac8c394e23e98e7
Jaun406 <152258555+Jaun406@users.noreply.github.com>
2025-09-01 19:04:36 +0200

    Add files via upload

---
+++ Newton method/Python/NewtonMethod.py
@@ -0,0 +1,35 @@
+def f(x) -> float:
+    return x**2 - 6*x + 5
+
+
+def x_prime(x) -> float:
+    return 2*x - 6
+
+
+def newton_method(x0, tol=1e-6, max_iter=100) -> float:
+    real_error: float = float('inf')
+    iter: int = max_iter
+
+    while iter > 0 or real_error > tol:
+        x1 = x0 - f(x0)/x_prime(x0)
+        iter -= 1
+        real_error = abs(x1-x0)
+        x0 = x1
+    return x1
+
+def main():
+    print('My function is: x**2 - 6x + 5')
+    print('Derivative: 2x - 6')
+    root = newton_method(float(input("Enter initial guess:")))
+    print('The root of this function is: '+root)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
